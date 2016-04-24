b2e8b7290b16e6419e36e49eaaf19a581f3fd0db
Zhang Cheng <13501393281@163.com>
2016-04-24 19:43:19 -0400

    update setup script

--- setup.ps1
+++ setup.ps1
@@ -2,4 +2,3 @@
 echo 'source ./vimfiles/idea_vim_config.vim' |Out-File $HOME\.ideavimrc
 echo 'source ./vimfiles/vs_vim_config.vim' |Out-File $HOME\_vsvimrc
 echo 'source ./vimfiles/gvim_config.vim' |Out-File $HOME\_vimrc
-Set-Location $location
