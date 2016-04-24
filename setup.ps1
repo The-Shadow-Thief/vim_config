8c5302c4c63790b48343f8a10b98efd9f2b74c39
Zhang Cheng <13501393281@163.com>
2016-04-24 19:50:02 -0400

    update setup script

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,7 @@
-﻿git clone git@github.com:chantisnake/vim_config.git .\vimfiles
+﻿$location = Get-Location
+Set-Location $HOME
+git clone git@github.com:chantisnake/vim_config.git .\vimfiles
 echo 'source ./vimfiles/idea_vim_config.vim' |Out-File $HOME\.ideavimrc
 echo 'source ./vimfiles/vs_vim_config.vim' |Out-File $HOME\_vsvimrc
 echo 'source ./vimfiles/gvim_config.vim' |Out-File $HOME\_vimrc
+Set-Location $location
