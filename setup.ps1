f794a8a99b35645da5f760baad1e4905ae092146
Zhang Cheng <13501393281@163.com>
2016-04-24 20:07:07 -0400

    continuing update setup script

--- setup.ps1
+++ setup.ps1
@@ -1,13 +1,13 @@
 ﻿$location = Get-Location
 Set-Location $HOME
 git clone git@github.com:chantisnake/vim_config.git .\vimfiles
-$stream = [System.IO.StreamWriter] ".ideavimrc"
+$stream = [System.IO.StreamWriter] "$HOME/.ideavimrc"
 $stream.Write('source ./vimfiles/idea_vim_config.vim')
 $stream.Close()
-$stream = [System.IO.StreamWriter] "_vimrc"
+$stream = [System.IO.StreamWriter] "$HOME/_vimrc"
 $stream.Write('source ./vimfiles/gvim_config.vim')
 $stream.Close()
-$stream = [System.IO.StreamWriter] "_vsvimrc"
+$stream = [System.IO.StreamWriter] "$HOME/_vsvimrc"
 $stream.Write('source ./vimfiles/vs_vim_config.vim')
 $stream.Close()
 Set-Location $location
