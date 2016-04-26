6ac26c7ec0379e50a93f29358c34b4ec0c9fdab2
Zhang Cheng <13501393281@163.com>
2016-04-25 20:22:14 -0400

    update setup script

--- setup.ps1
+++ setup.ps1
@@ -3,12 +3,11 @@ Write-Host 'set location to home'
 Set-Location $HOME
 git clone git@github.com:chantisnake/vim_config.git .\vimfiles
 $stream = [System.IO.StreamWriter] ".ideavimrc"
-$stream.Write('source ./vimfiles/idea_vim_config.vim')
+$stream.Write('source ~/vimfiles/idea_vim_config.vim')
 $stream.Close()
 $stream = [System.IO.StreamWriter] "_vimrc"
-$stream.Write('source ./vimfiles/gvim_config.vim')
+$stream.Write('source ~/vimfiles/gvim_config.vim')
 $stream.Close()
 $stream = [System.IO.StreamWriter] "_vsvimrc"
-$stream.Write('source ./vimfiles/vs_vim_config.vim')
+$stream.Write('source ~/vimfiles/vs_vim_config.vim')
 $stream.Close()
-Set-Location $location
